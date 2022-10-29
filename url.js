07c5f7acc4c3e7639813648f95c4fac3d2b1d3ff
sudharm jadhav <93636952+sudharm2004@users.noreply.github.com>
2022-10-29 11:38:14 +0530

    Update url.js

--- url.js
+++ url.js
@@ -1,7 +1,11 @@
-import url from 'url';
+const url=require('url');
+var adr = 'http://localhost:8080/default.htm?year=2017&month=february';
+var q = url.parse(adr, true);
 
-const myURL = new URL('https://example.org:8000');
-myURL.pathname = '/a/b/c';
-myURL.search = '?d=e';
-myURL.hash = '#fgh';
-console.log(myURL);
+console.log(q.host); //returns 'localhost:8080'
+console.log(q.pathname); //returns '/default.htm'
+console.log(q.search); //returns '?year=2017&month=february'
+
+var qdata=q.query;
+console.log(qdata);
+
